2d4a4e378235a3ee6e52b72b2b895bd12d7bd1ad
Bjornhona <asaeri3@hotmail.com>
2021-03-19 21:39:13 +0100

    both domains set

--- app.js
+++ app.js
@@ -43,11 +43,11 @@ app.use(cors({
   // optionsSuccessStatus: 204,
   // methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
   // preflightContinue: false,
-  headers: {
-    "Access-Control-Allow-Origin": "*",
+  // headers: {
+    // "Access-Control-Allow-Origin": "*",
     // "Access-Control-Allow-Headers": "Access-Control-Allow-Headers, Access-Control-Allow-Origin, Origin, Accept, Content-Type"
-    "Access-Control-Allow-Headers": "Access-Control-Allow-Headers, Origin, Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers, Access-Control-Allow-Origin"
-  }
+    // "Access-Control-Allow-Headers": "Access-Control-Allow-Headers, Origin, Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers, Access-Control-Allow-Origin"
+  // }
 }));
 
 // app.options('*', cors());
