330bd90d1a3544b225fa96bea884f9ac22c8f42a
Bjornhona <asaeri3@hotmail.com>
2021-03-21 13:52:40 +0100

    headers for access control set

--- app.js
+++ app.js
@@ -28,21 +28,16 @@ mongoose.connect(process.env.MONGODB_URI, {
 const app = express();
 const port = process.env.PORT || 5000;
 
-// app.all('/', function(req, res, next) {
-//   const allowedOrigins = ["https://beerio-aa491.web.app/", "https://beerio-api-eu.herokuapp.com/"];
-//   let origin = req.headers.origin;
-//   if (allowedOrigins.includes(origin)) {
-//     res.header("Access-Control-Allow-Origin", origin);
-//   }
-//   // res.header("Access-Control-Allow-Origin", "*");
+app.all('/', function(req, res, next) {
+  const allowedOrigins = ["https://beerio-aa491.web.app/", "https://beerio-api-eu.herokuapp.com/"];
+  let origin = req.headers.origin;
+  if (allowedOrigins.includes(origin)) {
+    res.header("Access-Control-Allow-Origin", origin);
+  }
+  // res.header("Access-Control-Allow-Origin", "*");
 //   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Accept");
-//   next();
-// });
-
-// app.use(cors({
-//   credentials: true,
-//   origin: [process.env.PUBLIC_DOMAIN],
-// }));
+  next();
+});
 
 app.get('/', (req, res) => {
   res.send({ express: 'Hello From Beerio API!' });
@@ -51,13 +46,10 @@ app.get('/', (req, res) => {
 app.use(favicon(__dirname + '/public/favicon.ico'));
 app.get('/favicon.ico', (req, res) => res.status(204));
 
-app.all('/', cors({
+app.use(cors({
   credentials: true,
   origin: [process.env.PUBLIC_DOMAIN],
-}, function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-}
-));
+}));
 
 app.use(session({
   store: new MongoStore({
