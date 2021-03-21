02b4dac9690691e64d6b7bd309f9375a5e50eac0
Bjornhona <asaeri3@hotmail.com>
2021-03-21 13:31:57 +0100

    headers for access control set

--- app.js
+++ app.js
@@ -29,7 +29,12 @@ const app = express();
 const port = process.env.PORT || 5000;
 
 app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
+  const allowedOrigins = ["https://beerio-aa491.web.app/", "https://beerio-api-eu.herokuapp.com/"];
+  let origin = req.headers.origin;
+  if (allowedOrigins.includes(origin)) {
+    res.header("Access-Control-Allow-Origin", origin); // restrict it to the required domain
+  }
+  // res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Accept");
   next();
 });
