5bf5c1e0b91cae5bac6e1a9b599b6e523f9852f4
Bjornhona <asaeri3@hotmail.com>
2021-03-21 11:46:58 +0100

    headers for access control set

--- app.js
+++ app.js
@@ -28,7 +28,7 @@ mongoose.connect(process.env.MONGODB_URI, {
 const app = express();
 const port = process.env.PORT || 5000;
 
-app.all('/auth/me', function(req, res, next) {
+app.all('/', function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
   // res.header("Access-Control-Allow-Headers", "X-Requested-With");
   next();
@@ -47,7 +47,8 @@ app.use(cors({
 }));
 
 // Then pass them to cors:
-app.use(cors(corsOptions));
+// app.use(cors(corsOptions));
+// app.use(cors()); 
 
 app.use(session({
   store: new MongoStore({
