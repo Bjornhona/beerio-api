6ee96463f495b33800f07c2d353dd9e12b2d9399
Bjornhona <asaeri3@hotmail.com>
2021-03-21 13:34:41 +0100

    headers for access control set

--- app.js
+++ app.js
@@ -28,11 +28,11 @@ mongoose.connect(process.env.MONGODB_URI, {
 const app = express();
 const port = process.env.PORT || 5000;
 
-app.use(function(req, res, next) {
+app.all('/', function(req, res, next) {
   const allowedOrigins = ["https://beerio-aa491.web.app/", "https://beerio-api-eu.herokuapp.com/"];
   let origin = req.headers.origin;
   if (allowedOrigins.includes(origin)) {
-    res.header("Access-Control-Allow-Origin", origin); // restrict it to the required domain
+    res.header("Access-Control-Allow-Origin", origin);
   }
   // res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Accept");
