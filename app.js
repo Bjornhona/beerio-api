30fb0aff8ffd38fd613fd85634d1411d2b5f5b3d
Bjornhona <asaeri3@hotmail.com>
2021-03-28 15:08:16 +0200

    again

--- app.js
+++ app.js
@@ -35,7 +35,7 @@ const app = express();
   // }
   // res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With");
 app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "https://beerio-aa491.web.app/");
+  res.header("Access-Control-Allow-Origin", "https://beerio-aa491.web.app");
   // res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
   next();
 });
@@ -50,11 +50,11 @@ app.get('/favicon.ico', (req, res) => res.status(204));
 
 app.use(cors({
   credentials: true,
-  origin: "https://beerio-aa491.web.app/"
+  origin: "https://beerio-aa491.web.app"
 }));
 // origin: [process.env.PUBLIC_DOMAIN]
 
-app.options("https://beerio-aa491.web.app/", cors());
+app.options("https://beerio-aa491.web.app", cors());
 // app.options('*', cors());
 
 app.use(session({
