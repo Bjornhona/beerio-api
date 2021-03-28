555abcc87344bc7dd6cf19cc8ba1485b8f7fd1f4
Bjornhona <asaeri3@hotmail.com>
2021-03-28 16:55:57 +0200

    again

--- app.js
+++ app.js
@@ -34,12 +34,12 @@ const app = express();
   //   res.header("Access-Control-Allow-Origin", origin);
   // }
   // res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With");
-app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "https://beerio-aa491.web.app");
-  res.header("Access-Control-Allow-Methods", GET, POST, PUT, DELETE, OPTIONS);
-  res.header("Access-Control-Allow-Credentials", "true")
-  next();
-});
+// app.use(function(req, res, next) {
+//   res.header("Access-Control-Allow-Origin", "https://beerio-aa491.web.app");
+//   res.header("Access-Control-Allow-Methods", GET, POST, PUT, DELETE, OPTIONS);
+//   res.header("Access-Control-Allow-Credentials", "true")
+//   next();
+// });
   // res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
 
 
@@ -51,8 +51,9 @@ app.get('/', (req, res) => {
 app.use(favicon(__dirname + '/public/favicon.ico'));
 app.get('/favicon.ico', (req, res) => res.status(204));
 
+// credentials: true,
+
 app.use(cors({
-  credentials: true,
   origin: [process.env.PUBLIC_DOMAIN]
 }));
 
