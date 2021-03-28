b84ce770b64922c51aa193d8023606e21735065f
Bjornhona <asaeri3@hotmail.com>
2021-03-28 16:00:08 +0200

    again

--- app.js
+++ app.js
@@ -35,10 +35,12 @@ const app = express();
   // }
   // res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With");
 app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "https://beerio-aa491.web.app/");
-  // res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  res.header("Access-Control-Allow-Origin", "https://beerio-aa491.web.app");
+  res.header("Access-Control-Allow-Methods", GET, POST, PUT, DELETE, OPTIONS);
   next();
 });
+  // res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+
 
 // res.header("Access-Control-Allow-Origin", "true");
 app.get('/', (req, res) => {
