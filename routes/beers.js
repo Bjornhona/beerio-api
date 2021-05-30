10ca6d6baab9bb55e8947167f37514ed8b6423d0
Bjornhona <asaeri3@hotmail.com>
2021-05-30 17:51:02 +0200

    resceiving beer search data

--- routes/beers.js
+++ routes/beers.js
@@ -23,9 +23,8 @@ router.get('/search/:query', (req, res, next) => {
 
   axios.get(`https://api.brewerydb.com/v2/search?q=${query}&type=beer&key=1ff4f5a771c204dd18912e145d2e13ac`)
   .then(result => {
-    response = result.data
-    // return res.json(response);
-    return result;
+    const response = result.data;
+    return res.json(response);
   })
   .catch(error => next(error));
 })
