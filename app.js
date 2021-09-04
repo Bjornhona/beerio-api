8fdda07672397756810555a9ab404e09fadc8b53
Bjornhona <asaeri3@hotmail.com>
2021-09-04 07:50:32 +0200

    cookie refactor

--- app.js
+++ app.js
@@ -45,16 +45,19 @@ app.use(session({
   secret: 'some-string',
   resave: true,
   saveUninitialized: true,
-  // cookie: {
-  //   maxAge: 24 * 60 * 60 * 1000
-  // }
-  cookie: { 
-    httpOnly: true, 
-    secure: true, 
-    maxAge: 1000 * 60 * 60 * 48, 
-    sameSite: 'none' 
+  cookie: {
+    maxAge: 24 * 60 * 60 * 1000,
+    sameSite: 'none'
   }
+  // cookie: { 
+  //   httpOnly: true, 
+  //   secure: true, 
+  //   maxAge: 1000 * 60 * 60 * 48, 
+  //   sameSite: 'none' 
+  // }
 }));
+
+// app.enable('trust proxy');
 
 app.use(logger('dev'));
 app.use(express.urlencoded({extended: true})); 
