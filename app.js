b0d6fe911cd1cd9360856ebd10e2c4129134121b
Bjornhona <asaeri3@hotmail.com>
2021-10-04 21:57:56 +0200

    ready for deploy

--- app.js
+++ app.js
@@ -47,7 +47,7 @@ app.use(session({
   saveUninitialized: true,
   cookie: {
     httpOnly: true,
-    secure: true, 
+    // secure: true, 
     maxAge: 24 * 60 * 60 * 1000,
     sameSite: 'none',
   },
