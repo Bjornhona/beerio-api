6e8be061a52a9d9c91873bb1bd12798e503c2ffb
Bjornhona <asaeri3@hotmail.com>
2024-03-11 08:20:12 +0100

    refactoring to modernize old code

--- app.js
+++ app.js
@@ -1,29 +1,40 @@
 const express = require('express');
+const mongoose = require('mongoose');
 const path = require('path');
 const favicon = require('serve-favicon');
 const logger = require('morgan');
-const mongoose = require('mongoose');
 const session = require('express-session');
-const MongoStore = require('connect-mongo')(session);
+const MongoStore = require('connect-mongo');
 const cors = require('cors');
 const bodyParser = require('body-parser');
-
-require('dotenv').config();
-
 const auth = require('./routes/auth');
 const beers = require('./routes/beers');
 
-mongoose.connect(process.env.MONGODB_URI, {
-  keepAlive: true,
-  useNewUrlParser: true,
-  useUnifiedTopology: true
-}).then(() => {
-  console.log(`Connected to database`);
-}).catch((error) => {
-  console.error(error);
-})
+require('dotenv').config();
 
 const app = express();
+
+const connect = async () => {
+  try {
+      await mongoose.connect(process.env.MONGODB_URI);
+      console.log("connected to MongoDB");
+  }
+  catch (error) {
+      console.error(error);
+  }
+}
+
+connect();
+
+// mongoose.connect(process.env.MONGODB_URI, {
+//   keepAlive: true,
+//   useNewUrlParser: true,
+//   useUnifiedTopology: true
+// })
+// .then(m => m.connection.getClient())
+// .catch((error) => {
+//   console.error(error);
+// })
 
 app.get('/', (req, res) => {
   return res.send({ express: 'Hello From Beerio API!' });
@@ -38,20 +49,49 @@ app.use(cors({
 }));
 
 app.use(session({
-  store: new MongoStore({
-    mongooseConnection: mongoose.connection,
-    ttl: 24 * 60 * 60 // 1 day
-  }),
+//   store: MongoStore.create({
+//     // mongoUrl: 
+//     clientPromise: mongoose.connection,
+//     // mongooseConnection: mongoose.connection,
+//     ttl: 24 * 60 * 60 // 1 day
+//   }),
+  // secret: 'some-string',
+//   resave: true,
+//   saveUninitialized: true,
+//   cookie: {
+//     httpOnly: true,
+//     secure: true, 
+//     maxAge: 24 * 60 * 60 * 1000,
+//     sameSite: 'none',
+//   },
+  name: 'example.sid',
   secret: 'some-string',
+  httpOnly: true,
+  secure: true,
+  maxAge: 1000 * 60 * 60 * 7,
   resave: true,
   saveUninitialized: true,
-  cookie: {
-    httpOnly: true,
-    secure: true, 
-    maxAge: 24 * 60 * 60 * 1000,
-    sameSite: 'none',
-  },
+  store: MongoStore.create({
+      mongoUrl: process.env.MONGODB_URI
+  })
 }));
+
+// app.post('/', async (req, res, next) => {
+//   const {name} = req.body;
+//   req.session.user = {
+//       name,
+//       isLoggedIn: true
+//   }
+
+//   try {
+//       await req.session.save();
+//   } catch (err) {
+//       console.error('Error saving to session storage: ', err);
+//       return next(new Error('Error creating user'));
+//   }
+
+//   res.status(200).send();
+// });
 
 app.enable('trust proxy');
 
@@ -63,19 +103,20 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/auth', auth);
 app.use('/beers', beers);
 
-// catch 404 and forward to error handler
 app.use((req, res, next) => {
   return res.status(404).json({ code: 'not found' });
 });
 
 app.use((err, req, res, next) => {
-  // always log the error
   console.error('ERROR', req.method, req.path, err);
 
-  // only render if the error ocurred before sending the response
   if (!res.headersSent) {
     return res.status(500).json({ code: 'unexpected' });
   }
+});
+
+app.listen(8080, () => {
+  console.log("server started on port 8080");
 });
 
 module.exports = app;
